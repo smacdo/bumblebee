ab122d5f25a7c4dafa45951cea7bcec7f4658f08
Scott MacDonald <scott@smacdo.com>
2022-05-08 21:20:50 -0700

    split into reusable library with cli exe

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@
 // limitations under the License.
 ////////////////////////////////////////////////////////////////////////////////
 // TODO: Windows support since it doesn't have a builtin dictionary?
+use bumblebee::find_all;
 use clap::Parser;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
@@ -76,102 +77,4 @@ fn find_all_with_dict<P: AsRef<Path>>(
         required,
         extra,
     ))
-}
-
-/// Find all valid answers given an iterable list of potential words.
-/// IntoIterator fix: https://stackoverflow.com/a/35626785
-fn find_all<I, S>(words: I, required: char, extra: &str) -> Vec<String>
-where
-    I: IntoIterator<Item = S>,
-    S: AsRef<str>,
-{
-    let mut answers: Vec<String> = Vec::new();
-
-    for w in words {
-        if is_valid_word(w.as_ref(), required, extra) {
-            answers.push(w.as_ref().to_string());
-        }
-    }
-
-    answers
-}
-
-/// Test if the given word `word` is a valid answer to the problem. A word is
-/// considered a solution if it is at least four letters long, at least one
-/// character matches `required`, and the remaining letters match either
-/// `required` or one of the values in `extra`.
-pub fn is_valid_word(word: &str, required: char, extra: &str) -> bool {
-    // Words must be at least four characters.
-    if word.len() < 4 {
-        return false;
-    }
-
-    // Words must also contain the required character.
-    if !word.contains(required) {
-        return false;
-    }
-
-    // Words can only contain characters matching required or extra.
-    word.chars()
-        .into_iter()
-        .all(|x| x == required || extra.chars().any(|e| e == x))
-}
-
-#[cfg(test)]
-#[allow(dead_code)]
-mod tests {
-    use crate::{find_all, is_valid_word};
-
-    #[test]
-    fn empty_word_is_not_valid() {
-        assert_eq!(false, is_valid_word("", 'c', "ab"));
-    }
-
-    #[test]
-    fn word_less_than_four_chars_not_valid() {
-        assert_eq!(false, is_valid_word("c", 'c', "ab"));
-        assert_eq!(false, is_valid_word("ca", 'c', "ab"));
-        assert_eq!(false, is_valid_word("cab", 'c', "ab"));
-    }
-
-    #[test]
-    fn word_uses_required_valid() {
-        assert_eq!(true, is_valid_word("tttt", 't', "oe"));
-        assert_eq!(true, is_valid_word("tttttt", 't', "oe"));
-        assert_eq!(true, is_valid_word("ssss", 's', "oe"));
-    }
-
-    #[test]
-    fn word_missing_required_not_valid() {
-        assert_eq!(false, is_valid_word("oeoe", 't', "oe"));
-        assert_eq!(false, is_valid_word("oooo", 't', "oe"));
-    }
-
-    #[test]
-    fn word_can_use_extra_chars() {
-        assert_eq!(true, is_valid_word("tote", 't', "elom"));
-        assert_eq!(true, is_valid_word("totet", 't', "elom"));
-        assert_eq!(true, is_valid_word("mote", 't', "elom"));
-        assert_eq!(true, is_valid_word("molet", 't', "elom"));
-    }
-
-    #[test]
-    fn word_with_chars_not_in_required_or_extra_not_valid() {
-        assert_eq!(false, is_valid_word("dote", 't', "elom"));
-        assert_eq!(false, is_valid_word("note", 't', "elom"));
-    }
-
-    #[test]
-    fn test_multiple_words() {
-        let words = vec![
-            "tote".to_string(),
-            "vote".to_string(),
-            "mote".to_string(),
-            "soapy".to_string(),
-        ];
-        let answers = find_all(words.iter(), 't', "elom");
-        assert_eq!(2, answers.len());
-        assert_eq!("tote", answers[0]);
-        assert_eq!("mote", answers[1]);
-    }
 }
